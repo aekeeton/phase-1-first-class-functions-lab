ab72e3abe50e1cff9725178bab976f6fe0d8775c
aekeeton <ashley.keeton@outlook.com>
2022-05-25 20:21:50 -0500

    commit tree 492dc56da4b4ca23d1629f62f87120a0817996dc

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(array) {
+    return [...array.slice(0, 2)];
+}
+
+const returnLastTwoDrivers = function(array) {
+    return [...array.slice(2)];
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fare) {
+    return function() {
+        return fare * 5
+    }
+}
+
+const fareMultiplier = createFareMultiplier(2);
+const fareQuintupler = createFareMultiplier(5);
+
+function fareDoubler(fare) {
+    return fare * 2
+}
+
+function fareTripler(fare) {
+    return fare * 3
+}
+
+const selectDifferentDrivers = (arrayOfDrivers, returnFirstTwoDrivers) => {
+    return returnFirstTwoDrivers(arrayOfDrivers);
+}
+
